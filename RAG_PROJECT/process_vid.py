fb76787dd61b5439ef8130292c5dd59060472fd4
Yash <yashatwood@gmail.com>
2026-06-15 13:05:25 +0530

    remove RAG_PROJECT, moving to separate repo

--- RAG_PROJECT/process_vid.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-files = os.listdir("videos")
-
-for file in files:
-    num = file.split("- ")[1].split(" ")[0]
-    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{num}.mp3"])
